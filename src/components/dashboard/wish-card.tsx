4f69276230721aea1f1524549f3b69ab15feb6e1
Roman Kostiv <kostiv.rr2005@gmail.com>
2025-12-05 17:09:47 +0200

    fix(wish-card): fix unexpected error

--- src/components/dashboard/wish-card.tsx
+++ src/components/dashboard/wish-card.tsx
@@ -7,14 +7,14 @@ const WishCard = ({ wish } : { wish: IWish }) => {
     return (
         <div className="rounded-lg shadow p-4 bg-card flex flex-col justify-end">
             <div className="mb-3 flex items-center justify-center overflow-hidden flex-1 aspect-square ">
-                {wish.imagePath ?
-                    <img src={wish.imagePath} alt={wish.title} className="object-cover h-full w-full rounded-2xl"/> :
+                {wish?.imagePath ?
+                    <img src={wish.imagePath} alt={wish?.title} className="object-cover h-full w-full rounded-2xl"/> :
                     <div className="text-gray-400">No image</div>}
             </div>
             <div>
                 <div className={"flex justify-between"}>
-                    <span className="font-semibold mb-1">{wish.title}</span>
-                    <span className="text-lg font-bold">${wish.price.toFixed(2)}</span>
+                    <span className="font-semibold mb-1">{wish?.title}</span>
+                    <span className="text-lg font-bold">${wish?.price}</span>
                 </div>
                 <p className="text-sm min-h-10 text-gray-600 mb-2">{wish.description}</p>
                 <div className=" flex items-center justify-between">
